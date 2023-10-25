b01879a9d0ee028c12a037a809ddd798882d1732
D.Kozlov <novolan2014@gmail.com>
2023-10-25 10:47:25 +0300

    remove imports

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import { loadData } from "./js/index.js"; 
+const loadData = async (url, requestConfig) => {
+    return fetch(url, requestConfig)
+       .then(function (response) {
+         if (response.ok) {
+           return response.json();
+         } else {
+           throw new Error("Request failed with status: " + response.status);
+         }
+       })
+       .then(function (data) {
+         return data
+       })
+       .catch(function (error) {
+         return error
+       });
+   }   
 
 const gender = document.getElementById('gender')
 const culture = document.getElementById('culture')
