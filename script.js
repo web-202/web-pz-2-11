8af9f09c8e79076818615eb4d4d7fdcedcfbbdbc
David <eldarmanzefirka@gmail.com>
2023-10-28 13:48:28 +0300

    (fixed) fix infinity scroll, fix default selection

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let nextPage = 1;
+let characterCount1 = 1;
+let isLoading = false;
 function httpRequest(type, url, callback) {
     const xhr = new XMLHttpRequest();
     xhr.open(type, url, true);
@@ -9,51 +12,61 @@ function httpRequest(type, url, callback) {
     };
     xhr.send();
 }
-let characterCount1 = 1; 
+function httpRequest(type, url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(type, url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            callback(response);
+        }
+    };
+    xhr.send();
+}
 function loadCharacters() {
     const characterList = document.getElementById("character-list");
-    let selectedCharacter = null;
-    httpRequest("GET", "https://anapioficeandfire.com/api/characters?page=1&pageSize=50", function (data) {
-        data.forEach(function (character) {
-            const listItem = document.createElement("li");
-            listItem.textContent = `Character ${characterCount1}`;
-            characterCount1++;
+    let nextPage = 1;
 
-            listItem.addEventListener("click", function () {
-                if (selectedCharacter) {
-                    selectedCharacter.classList.remove("selected");
-                }
+    function loadMoreCharacters() {
+        httpRequest("GET", `https://anapioficeandfire.com/api/characters?page=${nextPage}&pageSize=50`, function (data) {
+            if (data.length === 0) {
+                window.removeEventListener('scroll', scrollHandler);
+                return;
+            }
+            data.forEach(function (character) {
+                const listItem = document.createElement("li");
+                listItem.textContent = `Character ${characterList.children.length + 1}`;
 
-                selectedCharacter = listItem;
-                selectedCharacter.classList.add("selected");
+                listItem.addEventListener("click", function () {
+                    loadCharacterDetails(character.url);
+                });
 
-                loadCharacterDetails(character.url);
+                characterList.appendChild(listItem);
             });
-
-            listItem.addEventListener("mouseover", function () {
-                if (listItem !== selectedCharacter) {
-                    listItem.style.backgroundColor = "#f0f0f0"; 
-                }
-            });
-
-            listItem.addEventListener("mouseout", function () {
-                if (listItem !== selectedCharacter) {
-                    listItem.style.backgroundColor = "transparent";
-                }
-            });
-
-            characterList.appendChild(listItem);
+            nextPage++;
         });
-        loadCharacterDetails(data[0].url);
-    });
+    }
+
+    loadMoreCharacters();
+
+    function scrollHandler() {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            loadMoreCharacters();
+        }
+    }
+
+    window.addEventListener('scroll', scrollHandler);
 }
-let characterCount = 1;
 function loadCharacterDetails(characterUrl) {
     const characterDetails = document.getElementById("character-details");
-        httpRequest("GET", characterUrl, function (data) {
-        let html = `<h2>Character ${characterCount}</h2>`;
-        characterCount++; 
-        html += `<p>Name: ${data.name || "Unknown"}</p>`;
+    httpRequest("GET", characterUrl, function (data) {
+    });
+}
+
+function loadCharacterDetails(characterUrl) {
+    const characterDetails = document.getElementById("character-details");
+    httpRequest("GET", characterUrl, function (data) {
+        let html = `<p>Name: ${data.name || "Unknown"}</p>`;
         html += `<p>Gender: ${data.gender || "Unknown"}</p>`;
         html += `<p>Culture: ${data.culture || "Unknown"}</p>`;
         html += `<p>Born: ${data.born || "Unknown"}</p>`;
@@ -72,38 +85,7 @@ function loadCharacterDetails(characterUrl) {
             });
             html += "</ul>";
         }
-        if (data.allegiances && data.allegiances.length > 0) {
-            html += "<p>Allegiances:</p><ul>";
-            data.allegiances.forEach(allegianceUrl => {
-            });
-            html += "</ul>";
-        }
-        if (data.books && data.books.length > 0) {
-            html += "<p>Appeared in Books:</p><ul>";
-            data.books.forEach(bookUrl => {
-            });
-            html += "</ul>";
-        }
-        if (data.povBooks && data.povBooks.length > 0) {
-            html += "<p>Point of View in Books:</p><ul>";
-            data.povBooks.forEach(bookUrl => {
-            });
-            html += "</ul>";
-        }
-        if (data.tvSeries && data.tvSeries.length > 0) {
-            html += "<p>Appeared in TV Series:</p><ul>";
-            data.tvSeries.forEach(series => {
-                html += `<li>${series}</li>`;
-            });
-            html += "</ul>";
-        }
-        if (data.playedBy && data.playedBy.length > 0) {
-            html += "<p>Played By:</p><ul>";
-            data.playedBy.forEach(actor => {
-                html += `<li>${actor}</li>`;
-            });
-            html += "</ul>";
-        }
+
         characterDetails.innerHTML = html;
     });
 }
